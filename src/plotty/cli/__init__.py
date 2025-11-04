f8bb2b0788cae34d38f7f7a36ebc46041373f8df
Bernardo Kuri <github@bkuri.com>
2025-11-04 17:00:58 -0600

    reorder menu

--- src/plotty/cli/__init__.py
+++ src/plotty/cli/__init__.py
@@ -6,26 +6,21 @@ This package contains the main CLI interface split into logical command groups.
 
 from __future__ import annotations
 
-import typer
 from importlib import metadata
 
-from .add import add_app
-from .remove import remove_app
-from .list import list_app
+import typer
 
-# Import check and info commands
+from .add import add_app
 from .check import check_app
 from .info import status_app
-
-# Import system command group
-from .system import system_app
-
-# Import main level commands
-from .list.setup_wizard import setup
 from .interactive import interactive_command
-from .job_commands import start_command, optimize_command
-from .resume import resume_command
+from .job_commands import optimize_command, start_command
+from .list import list_app
+from .list.setup_wizard import setup
+from .remove import remove_app
 from .restart import restart_command
+from .resume import resume_command
+from .system import system_app
 
 # Get version
 try:
@@ -36,23 +31,19 @@ except metadata.PackageNotFoundError:
 # Create main app
 app = typer.Typer(no_args_is_help=True)
 
-# Add main level commands
-app.command("setup", help="Run setup wizard")(setup)
-app.command("interactive", help="Start interactive plotting session")(
-    interactive_command
-)
-app.command("start", help="Begin plotting a job")(start_command)
+# Add sub-apps and commands (alphabetical order)
+app.add_typer(add_app, name="add", help="Add new files")
+app.add_typer(check_app, name="check", help="System and device checking")
+app.add_typer(status_app, name="info", help="Status and monitoring commands")
+app.command("interactive", help="Start an interactive plot")(interactive_command)
+app.add_typer(list_app, name="list", help="List and manage resources")
 app.command("optimize", help="Optimize jobs for plotting")(optimize_command)
+app.add_typer(remove_app, name="remove", help="Remove resources")
 app.command("resume", help="Resume interrupted plotting jobs")(resume_command)
 app.command("restart", help="Restart job from beginning")(restart_command)
-
-# Add sub-apps (alphabetical order)
-app.add_typer(add_app, name="add", help="Add new resources")
-app.add_typer(check_app, name="check", help="System and device checking")
-app.add_typer(list_app, name="list", help="List and manage resources")
-app.add_typer(remove_app, name="remove", help="Remove resources")
+app.command("setup", help="Run setup wizard")(setup)
+app.command("start", help="Start plotting a job")(start_command)
 app.add_typer(system_app, name="system", help="System management commands")
-app.add_typer(status_app, name="info", help="Status and monitoring commands")
 
 
 @app.callback()
